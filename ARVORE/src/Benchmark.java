56cf97d7d4649e1c8f60ebcf6bbe32d2839c4b3c
PlanT50 <omegadarkxyz@gmail.com>
2026-05-24 18:26:39 -0300

    Objeto regras modificado para rules(só pra ficar a maioria em ingles)

--- ARVORE/src/Benchmark.java
+++ ARVORE/src/Benchmark.java
@@ -1,33 +1,21 @@
 public class Benchmark {
 
     private static final long SEED = 12345L;
-    private static final int NUM_REGRAS = 100000;
+    private static final int NUM_RULES = 100000;
 
 
     public static void main(String[] args) {
-        PacketRule[] regras = generateRules();
+        PacketRule[] rules = generateRules();
 
         AVLRouterTree avl = new AVLRouterTree();
         RedBlackRouterTree rbt = new RedBlackRouterTree();
 
 
-        long  insertionInitialAvl = System.nanoTime();
-        
-
-        for (PacketRule regra : regras){
-            avl.insert(regra);
-        }
-
-        long  insertionFinalAvl = System.nanoTime();
-        long  insertionTimeAvl = insertionFinalAvl - insertionInitialAvl;
-
-
-
         long  insertionInitialRbt = System.nanoTime();
         
 
-        for (PacketRule regra : regras){
-            rbt.insert(regra);
+        for (PacketRule regras : rules){
+            rbt.insert(regras);
         }
 
         long  insertionFinalRbt = System.nanoTime();
@@ -36,7 +24,6 @@ public class Benchmark {
         
 
 
-        System.out.println("Tempo consumido para inserção AVL: " + insertionTimeAvl + "ns");
         System.out.println("Tempo consumido para inserção RBT: " + insertionTimeRbt + "ns");
 
     
@@ -46,8 +33,8 @@ public class Benchmark {
         long  insertionInitialAvl = System.nanoTime();
         
 
-        for (PacketRule regra : regras){
-            avl.insert(regra);
+        for (PacketRule rules : regras){
+            avl.insert(rules);
         }
 
         long  insertionFinalAvl = System.nanoTime();
@@ -56,9 +43,9 @@ public class Benchmark {
     }
 
     private static PacketRule[] generateRules(){
-            PacketRule[] rules = new PacketRule[NUM_REGRAS];
+            PacketRule[] rules = new PacketRule[NUM_RULES];
 
-            for (int i = 0; i < NUM_REGRAS; i++){
+            for (int i = 0; i < NUM_RULES; i++){
                 int id = i + 1;
                 String ipOrigem = "10.0.0." + (id % 255);
                 String ipDestino = "172.16.0." + (id % 255);

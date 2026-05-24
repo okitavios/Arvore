bec40a450583fa24e727817e72645db95e44a2f9
PlanT50 <omegadarkxyz@gmail.com>
2026-05-24 16:08:03 -0300

    Adiconado constantes da seed a qual os testes serão executados e o tanto de teste que serão executados

--- ARVORE/src/Benchmark.java
+++ ARVORE/src/Benchmark.java
@@ -1,5 +1,7 @@
 public class Benchmark {
     public static void main(String[] args) {
+        private static final long SEED = 12345L;
+        private static final int NUM_TESTS = 100000;
        
     }
 }

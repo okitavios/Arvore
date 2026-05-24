99b696678f06f52370690e3234484b93a7b7a314
PlanT50 <omegadarkxyz@gmail.com>
2026-05-24 16:31:30 -0300

    Loop for adicionado para passar pelas 100000 regras, e identificar cada uma

--- ARVORE/src/Benchmark.java
+++ ARVORE/src/Benchmark.java
@@ -8,6 +8,13 @@ public class Benchmark {
 
         private static PacketRule[] generateRules(){
             PacketRule[] rules = new PacketRule[NUM_REGRAS];
+
+            for (int i = 0; i < NUM_REGRAS; i++){
+                int id = i + 1;
+                String ipOrigem = "10.0.0." + (id % 255);
+                String ipDestino = "172.16.0." + (id % 255);
+                int prioridade = id;
+            }
   
             return rules;
         }

2e13df251da0860f61fe34ab31eee9f024bfd86c
okitavios <okitavios1801@gmail.com>
2026-05-24 18:11:41 -0300

    segundo comit

--- ARVORE/src/RedBlackRouterTree.java
+++ ARVORE/src/RedBlackRouterTree.java
@@ -185,4 +185,204 @@ public class RedBlackRouterTree {
 
         return null;
     }
+
+    // =========================
+    // REMOÇÃO
+    // =========================
+
+    public void remove(PacketRule key) {
+
+        Node z = root;
+
+        while (z != null) {
+
+            int cmp = key.compareTo(z.data);
+
+            if (cmp == 0)
+                break;
+
+            z = (cmp < 0) ? z.left : z.right;
+        }
+
+        if (z == null)
+            return;
+
+        deleteNode(z);
+    }
+
+    private void deleteNode(Node z) {
+
+        Node y = z;
+        boolean yOriginalColor = y.color;
+
+        Node x;
+
+        if (z.left == null) {
+
+            x = z.right;
+            transplant(z, z.right);
+
+        } else if (z.right == null) {
+
+            x = z.left;
+            transplant(z, z.left);
+
+        } else {
+
+            y = minimum(z.right);
+            yOriginalColor = y.color;
+
+            x = y.right;
+
+            if (y.parent == z) {
+
+                if (x != null)
+                    x.parent = y;
+
+            } else {
+
+                transplant(y, y.right);
+
+                y.right = z.right;
+                y.right.parent = y;
+            }
+
+            transplant(z, y);
+
+            y.left = z.left;
+            y.left.parent = y;
+
+            y.color = z.color;
+        }
+
+        if (yOriginalColor == BLACK)
+            fixDelete(x);
+    }
+
+    private void transplant(Node u, Node v) {
+
+        if (u.parent == null)
+            root = v;
+
+        else if (u == u.parent.left)
+            u.parent.left = v;
+
+        else
+            u.parent.right = v;
+
+        if (v != null)
+            v.parent = u.parent;
+    }
+
+    private Node minimum(Node node) {
+
+        while (node.left != null)
+            node = node.left;
+
+        return node;
+    }
+
+    private void fixDelete(Node x) {
+
+        while (x != root && getColor(x) == BLACK) {
+
+            if (x == x.parent.left) {
+
+                Node w = x.parent.right;
+
+                if (getColor(w) == RED) {
+
+                    w.color = BLACK;
+                    x.parent.color = RED;
+
+                    rotateLeft(x.parent);
+
+                    w = x.parent.right;
+                }
+
+                if (getColor(w.left) == BLACK &&
+                        getColor(w.right) == BLACK) {
+
+                    w.color = RED;
+                    x = x.parent;
+
+                } else {
+
+                    if (getColor(w.right) == BLACK) {
+
+                        if (w.left != null)
+                            w.left.color = BLACK;
+
+                        w.color = RED;
+
+                        rotateRight(w);
+
+                        w = x.parent.right;
+                    }
+
+                    w.color = x.parent.color;
+                    x.parent.color = BLACK;
+
+                    if (w.right != null)
+                        w.right.color = BLACK;
+
+                    rotateLeft(x.parent);
+
+                    x = root;
+                }
+
+            } else {
+
+                Node w = x.parent.left;
+
+                if (getColor(w) == RED) {
+
+                    w.color = BLACK;
+                    x.parent.color = RED;
+
+                    rotateRight(x.parent);
+
+                    w = x.parent.left;
+                }
+
+                if (getColor(w.right) == BLACK &&
+                        getColor(w.left) == BLACK) {
+
+                    w.color = RED;
+                    x = x.parent;
+
+                } else {
+
+                    if (getColor(w.left) == BLACK) {
+
+                        if (w.right != null)
+                            w.right.color = BLACK;
+
+                        w.color = RED;
+
+                        rotateLeft(w);
+
+                        w = x.parent.left;
+                    }
+
+                    w.color = x.parent.color;
+                    x.parent.color = BLACK;
+
+                    if (w.left != null)
+                        w.left.color = BLACK;
+
+                    rotateRight(x.parent);
+
+                    x = root;
+                }
+            }
+        }
+
+        if (x != null)
+            x.color = BLACK;
+    }
+
+    private boolean getColor(Node node) {
+        return node == null ? BLACK : node.color;
+    }
 }

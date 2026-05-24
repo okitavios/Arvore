2b9e6474c5bf932a8f8e30b6c58c92d71b816584
PlanT50 <omegadarkxyz@gmail.com>
2026-05-24 16:14:09 -0300

    Nome de uma variavel mudada para melhor apresentação e escopo das variaveis mudadas

--- ARVORE/src/Benchmark.java
+++ ARVORE/src/Benchmark.java
@@ -1,8 +1,14 @@
 public class Benchmark {
+
+    private static final long SEED = 12345L;
+    private static final int NUM_REGRAS = 100000;
+
+
     public static void main(String[] args) {
-        private static final long SEED = 12345L;
-        private static final int NUM_TESTS = 100000;
-       
+        
+
+
+    
     }
 }
 

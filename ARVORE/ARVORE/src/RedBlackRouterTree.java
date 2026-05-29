cf6053524529680dfdc536209b9e376117fb00cd
PlanT50 <omegadarkxyz@gmail.com>
2026-05-28 22:19:19 -0300

    Merge

--- ARVORE/ARVORE/src/RedBlackRouterTree.java
+++ ARVORE/ARVORE/src/RedBlackRouterTree.java
@@ -1,12 +1,9 @@
-
-
 public class RedBlackRouterTree {
 
     private static final boolean RED = true;
     private static final boolean BLACK = false;
 
     private class Node {
-
         PacketRule data;
         Node left, right, parent;
         boolean color;
@@ -17,372 +14,234 @@ public class RedBlackRouterTree {
         }
     }
 
-    private Node root;
+    // Nó sentinela: representa todas as folhas nulas (sempre BLACK)
+    private final Node NIL = new Node(null);
 
+    private Node root;
     private int contadorRotacoes = 0;
+
+    public RedBlackRouterTree() {
+        NIL.color = BLACK;
+        NIL.left = NIL;
+        NIL.right = NIL;
+        NIL.parent = NIL;
+        root = NIL;
+    }
 
     public int getContadorRotacoes() {
         return contadorRotacoes;
     }
 
     private void rotateLeft(Node x) {
-
         contadorRotacoes++;
-
         Node y = x.right;
-
         x.right = y.left;
-
-        if (y.left != null)
-            y.left.parent = x;
-
+        if (y.left != NIL) y.left.parent = x;
         y.parent = x.parent;
-
-        if (x.parent == null)
-            root = y;
-
-        else if (x == x.parent.left)
-            x.parent.left = y;
-
-        else
-            x.parent.right = y;
-
+        if (x.parent == NIL)       root = y;
+        else if (x == x.parent.left) x.parent.left = y;
+        else                         x.parent.right = y;
         y.left = x;
         x.parent = y;
     }
 
     private void rotateRight(Node y) {
-
         contadorRotacoes++;
-
         Node x = y.left;
-
         y.left = x.right;
-
-        if (x.right != null)
-            x.right.parent = y;
-
+        if (x.right != NIL) x.right.parent = y;
         x.parent = y.parent;
-
-        if (y.parent == null)
-            root = x;
-
-        else if (y == y.parent.right)
-            y.parent.right = x;
-
-        else
-            y.parent.left = x;
-
+        if (y.parent == NIL)       root = x;
+        else if (y == y.parent.right) y.parent.right = x;
+        else                          y.parent.left = x;
         x.right = y;
         y.parent = x;
     }
 
     public void insert(PacketRule data) {
-
         Node node = new Node(data);
+        node.left = NIL;
+        node.right = NIL;
+        node.color = RED;
 
-        Node parent = null;
+        Node parent = NIL;
         Node current = root;
 
-        while (current != null) {
-
+        while (current != NIL) {
             parent = current;
-
-            if (data.compareTo(current.data) < 0)
-                current = current.left;
-
-            else
-                current = current.right;
+            if (data.compareTo(current.data) < 0) current = current.left;
+            else                                   current = current.right;
         }
 
         node.parent = parent;
-
-        if (parent == null)
-            root = node;
-
-        else if (data.compareTo(parent.data) < 0)
-            parent.left = node;
-
-        else
-            parent.right = node;
+        if (parent == NIL)                        root = node;
+        else if (data.compareTo(parent.data) < 0) parent.left = node;
+        else                                       parent.right = node;
 
         fixInsert(node);
     }
 
     private void fixInsert(Node z) {
-
-        while (z.parent != null && z.parent.color == RED) {
-
+        while (z.parent.color == RED) {
             Node gp = z.parent.parent;
-
             if (z.parent == gp.left) {
-
                 Node y = gp.right;
-
-                if (y != null && y.color == RED) {
-
+                if (y.color == RED) {           // Caso 1
                     z.parent.color = BLACK;
                     y.color = BLACK;
                     gp.color = RED;
                     z = gp;
-
                 } else {
-
-                    if (z == z.parent.right) {
+                    if (z == z.parent.right) {  // Caso 2
                         z = z.parent;
                         rotateLeft(z);
                     }
-
-                    z.parent.color = BLACK;
+                    z.parent.color = BLACK;     // Caso 3
                     gp.color = RED;
-
                     rotateRight(gp);
                 }
-
             } else {
-
                 Node y = gp.left;
-
-                if (y != null && y.color == RED) {
-
+                if (y.color == RED) {
                     z.parent.color = BLACK;
                     y.color = BLACK;
                     gp.color = RED;
-
                     z = gp;
-
                 } else {
-
                     if (z == z.parent.left) {
                         z = z.parent;
                         rotateRight(z);
                     }
-
                     z.parent.color = BLACK;
                     gp.color = RED;
-
                     rotateLeft(gp);
                 }
             }
         }
-
         root.color = BLACK;
     }
 
     public PacketRule search(PacketRule key) {
-
         Node current = root;
-
-        while (current != null) {
-
+        while (current != NIL) {
             int cmp = key.compareTo(current.data);
-
-            if (cmp == 0)
-                return current.data;
-
+            if (cmp == 0)    return current.data;
             current = (cmp < 0) ? current.left : current.right;
         }
-
         return null;
     }
 
-    // =========================
-    // REMOÇÃO
-    // =========================
-
     public void remove(PacketRule key) {
-
         Node z = root;
-
-        while (z != null) {
-
+        while (z != NIL) {
             int cmp = key.compareTo(z.data);
-
-            if (cmp == 0)
-                break;
-
+            if (cmp == 0) break;
             z = (cmp < 0) ? z.left : z.right;
         }
-
-        if (z == null)
-            return;
-
+        if (z == NIL) return;
         deleteNode(z);
     }
 
     private void deleteNode(Node z) {
-
         Node y = z;
         boolean yOriginalColor = y.color;
-
         Node x;
 
-        if (z.left == null) {
-
+        if (z.left == NIL) {
             x = z.right;
             transplant(z, z.right);
-
-        } else if (z.right == null) {
-
+        } else if (z.right == NIL) {
             x = z.left;
             transplant(z, z.left);
-
         } else {
-
             y = minimum(z.right);
             yOriginalColor = y.color;
-
             x = y.right;
 
             if (y.parent == z) {
-
-                if (x != null)
-                    x.parent = y;
-
+                x.parent = y;               // funciona mesmo se x == NIL
             } else {
-
                 transplant(y, y.right);
-
+                x = y.right;
                 y.right = z.right;
                 y.right.parent = y;
             }
 
             transplant(z, y);
-
             y.left = z.left;
             y.left.parent = y;
-
             y.color = z.color;
         }
 
         if (yOriginalColor == BLACK)
-            fixDelete(x);
+            fixDelete(x);                   // x nunca é null; pode ser NIL
     }
 
     private void transplant(Node u, Node v) {
-
-        if (u.parent == null)
-            root = v;
-
-        else if (u == u.parent.left)
-            u.parent.left = v;
-
-        else
-            u.parent.right = v;
-
-        if (v != null)
-            v.parent = u.parent;
+        if (u.parent == NIL)       root = v;
+        else if (u == u.parent.left) u.parent.left = v;
+        else                         u.parent.right = v;
+        v.parent = u.parent;        // seguro: v pode ser NIL, NIL.parent é válido
     }
 
     private Node minimum(Node node) {
-
-        while (node.left != null)
+        while (node.left != NIL)
             node = node.left;
-
         return node;
     }
 
     private void fixDelete(Node x) {
-
-        while (x != root && getColor(x) == BLACK) {
-
+        while (x != root && x.color == BLACK) {
             if (x == x.parent.left) {
-
                 Node w = x.parent.right;
-
-                if (getColor(w) == RED) {
-
+                if (w.color == RED) {                           // Caso 1
                     w.color = BLACK;
                     x.parent.color = RED;
-
                     rotateLeft(x.parent);
-
                     w = x.parent.right;
                 }
-
-                if (getColor(w.left) == BLACK &&
-                        getColor(w.right) == BLACK) {
-
+                if (w.left.color == BLACK && w.right.color == BLACK) { // Caso 2
                     w.color = RED;
                     x = x.parent;
-
                 } else {
-
-                    if (getColor(w.right) == BLACK) {
-
-                        if (w.left != null)
-                            w.left.color = BLACK;
-
+                    if (w.right.color == BLACK) {               // Caso 3
+                        w.left.color = BLACK;
                         w.color = RED;
-
                         rotateRight(w);
-
                         w = x.parent.right;
                     }
-
-                    w.color = x.parent.color;
+                    w.color = x.parent.color;                   // Caso 4
                     x.parent.color = BLACK;
-
-                    if (w.right != null)
-                        w.right.color = BLACK;
-
+                    w.right.color = BLACK;
                     rotateLeft(x.parent);
-
                     x = root;
                 }
-
             } else {
-
                 Node w = x.parent.left;
-
-                if (getColor(w) == RED) {
-
+                if (w.color == RED) {
                     w.color = BLACK;
                     x.parent.color = RED;
-
                     rotateRight(x.parent);
-
                     w = x.parent.left;
                 }
-
-                if (getColor(w.right) == BLACK &&
-                        getColor(w.left) == BLACK) {
-
+                if (w.right.color == BLACK && w.left.color == BLACK) {
                     w.color = RED;
                     x = x.parent;
-
                 } else {
-
-                    if (getColor(w.left) == BLACK) {
-
-                        if (w.right != null)
-                            w.right.color = BLACK;
-
+                    if (w.left.color == BLACK) {
+                        w.right.color = BLACK;
                         w.color = RED;
-
                         rotateLeft(w);
-
                         w = x.parent.left;
                     }
-
                     w.color = x.parent.color;
                     x.parent.color = BLACK;
-
-                    if (w.left != null)
-                        w.left.color = BLACK;
-
+                    w.left.color = BLACK;
                     rotateRight(x.parent);
-
                     x = root;
                 }
             }
         }
-
-        if (x != null)
-            x.color = BLACK;
-    }
-
-    private boolean getColor(Node node) {
-        return node == null ? BLACK : node.color ;
+        x.color = BLACK;
     }
 }
